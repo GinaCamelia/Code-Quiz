3e67daab2ae380a48b858b24e58c4e42945b6467
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-01-24 11:27:04 +0000

    initialized variables, created 2 functions: one that starts the quiz and one that updates the question and the answers on the screen and each answer is a button to choose from

--- assets/javaScript/codelogic.js
+++ assets/javaScript/codelogic.js
@@ -1,9 +1,39 @@
 let timeId = document.querySelector('#time');
-let startQuiz = document.querySelector('#start');
+let startBtn = document.querySelector('#start');
 let questionsId = document.querySelector('#questions');
-let choices = document.querySelector('#choices');
+let choicesId = document.querySelector('#choices');
 let userInitials = document.querySelector('#initials');
 let submitBtn = document.querySelector('#submit');
 let feedbackUser = document.querySelector('#feedback');
 
 
+let currentQuestion = 0;
+let remainingTime = questions.length * 15;
+let timeInterval;
+
+// Start the quiz
+function startQuiz(){
+    let startScreen = document.querySelector('#start-screen');
+    startScreen.style.display = 'none';
+    // console.log(startScreen);
+    questionsId.removeAttribute('class');
+    showQuestion();
+    timeInterval = setInterval(updateTime, 1000);
+}
+
+// Show the current question and answers
+function showQuestion() {
+    const questionTitle = document.querySelector('#question-title');
+    questionTitle.textContent = questions[currentQuestion].question;
+    choicesId.innerHTML = '';
+
+    for (let i = 0; i < questions[currentQuestion].answer.length; i++) {
+        const button = document.createElement('button');
+        button.textContent = questions[currentQuestion].answer[i];
+        button.addEventListener('click', checkAnswer);
+        choicesId.appendChild(button);
+    }
+}
+
+
+startBtn.addEventListener('click', startQuiz);
