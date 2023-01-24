167c44add7b7f6a2f6b4826566348cfd76f1346f
GinaCamelia <92101771+GinaCamelia@users.noreply.github.com>
2023-01-24 11:00:42 +0000

    stored id in variables

---
+++ assets/javaScript/codelogic.js
@@ -0,0 +1,9 @@
+let timeId = document.querySelector('#time');
+let startQuiz = document.querySelector('#start');
+let questionsId = document.querySelector('#questions');
+let choices = document.querySelector('#choices');
+let userInitials = document.querySelector('#initials');
+let submitBtn = document.querySelector('#submit');
+let feedbackUser = document.querySelector('#feedback');
+
+
